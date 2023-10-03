2a64c8971b3309bad6742a01125ce0e318b76f51
EstebanVera3741 <estebanvera3741@gmail.com>
2023-10-03 11:04:13 -0500

    start Project MessagingAsync

---
+++ src/main/java/MassagingAsync/MassagingAsyncApplication.java
@@ -0,0 +1,13 @@
+package MassagingAsync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MassagingAsyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MassagingAsyncApplication.class, args);
+	}
+
+}
---
+++ src/test/java/MassagingAsync/MassagingAsyncApplicationTests.java
@@ -0,0 +1,13 @@
+package MassagingAsync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MassagingAsyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
